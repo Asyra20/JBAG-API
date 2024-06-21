aa8472e837e36ec9250421f43c28e8ea5bd01ed8
z4fL <zaaaafl654@gmail.com>
2024-06-21 20:18:02 +0700

    install api

---
+++ app/Http/Resources/ResponseResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ResponseResource extends JsonResource
+{
+    public $status;
+    public $message;
+    public $resource;
+
+    public function __construct($status, $message, $resource)
+    {
+        parent::__construct($resource);
+        $this->status  = $status;
+        $this->message = $message;
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'success'   => $this->status,
+            'message'   => $this->message,
+            'data'      => $this->resource
+        ];
+    }
+}
