15570f8eb121f5e29ce881aed2bdc3a5bb010022
z4fL <zaaaafl654@gmail.com>
2024-06-22 16:32:25 +0700

    Tabel Transaksi, detail dan keranjang done?

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
-use App\Http\Controllers\KeranjangController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/keranjang/user/{id}', [KeranjangController::class, 'index']);
-Route::post('/keranjang', [KeranjangController::class, 'store']);
-Route::delete('/keranjang/{id}', [KeranjangController::class, 'delete']);
+Route::get('/', function () {
+    return view('welcome');
+});
