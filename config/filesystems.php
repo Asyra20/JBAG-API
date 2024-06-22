6a38a8e6cd2a8ff4db1d0406192021b684f1a33f
z4fL <zaaaafl654@gmail.com>
2024-06-22 16:47:05 +0700

    update file sistem

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -70,8 +70,8 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
-        public_path('images') => storage_path('app/images'),
+        // public_path('storage') => storage_path('app/public'),
+        storage_path('app/images') => public_path('images'),
     ],
 
 ];
