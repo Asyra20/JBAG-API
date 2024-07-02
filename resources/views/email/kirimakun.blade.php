0a6d2807fa76b051178d145af49535a8d7f11499
z4fL <zaaaafl654@gmail.com>
2024-07-02 17:38:06 +0700

    Perbaikan Kirim Akun Fixes #24

--- resources/views/email/kirimakun.blade.php
+++ resources/views/email/kirimakun.blade.php
@@ -1,3 +1,5 @@
+@use('Illuminate\Support\Number')
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,9 +9,12 @@
     <title>JBAG</title>
 </head>
 
-<body style="width: 100%; background-color: #FFC639; font-family: 'Helvetica', sans-serif; display: flex; justify-content: center; align-items: center; margin: 0;">
-    <div class="card-container" style="background-color: #131A2A; padding: 30px; margin-top: 30px; margin-left: auto; margin-right: auto; margin-bottom: 30px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); width: 550px; text-align: center;">
-        <div class="card-header" style="font-size: 38px; color: #FFFAFF; font-family: 'Helvetica', sans-serif; margin-bottom: 20px;">
+<body
+    style="width: 100%; background-color: #FFC639; font-family: 'Helvetica', sans-serif; display: flex; justify-content: center; align-items: center; margin: 0;">
+    <div class="card-container"
+        style="background-color: #131A2A; padding: 30px; margin-top: 30px; margin-left: auto; margin-right: auto; margin-bottom: 30px; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); width: 550px; text-align: center;">
+        <div class="card-header"
+            style="font-size: 38px; color: #FFFAFF; font-family: 'Helvetica', sans-serif; margin-bottom: 20px;">
             JBAG
         </div>
         <div class="card-body" style="font-family: 'Helvetica', sans-serif; color: #FFFAFF;">
@@ -18,45 +23,73 @@
             </div>
 
             @foreach ($data as $item)
-            <div class="divider" style="width: 100%; height: 1px; background-color: #FFC639; margin: 20px 0;"></div>
-            <div class="list-item" style="margin: 10px 0; display: flex; align-items: center; justify-content: start; font-size: 16px; vertical-align: middle; overflow: hidden;">
-                <span style="display: inline-block; width: 80px; font-size: 18px; margin-right: 10px; text-align: left; vertical-align: middle; white-space: nowrap;">
-                    Judul
-                </span>
-                <div style="margin-right: 5px; vertical-align: middle;">:</div>
-                <p style="flex: 1; margin: 0; text-align: left; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
-                    {{ $item['judul'] }}
-                </p>
-            </div>
-            <div class="list-item" style="margin: 10px 0; display: flex; align-items: center; justify-content: start; font-size: 16px; vertical-align: middle; overflow: hidden;">
-                <span style="display: inline-block; width: 80px; font-size: 18px; margin-right: 10px; text-align: left; vertical-align: middle; white-space: nowrap;">
-                    UID
-                </span>
-                <div style="margin-right: 5px; vertical-align: middle;">:</div>
-                <p style="flex: 1; margin: 0; text-align: left; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
-                    {{ $item['uid_akun'] }}
-                </p>
-            </div>
-            <div class="list-item" style="margin: 10px 0; display: flex; align-items: center; justify-content: start; font-size: 16px; vertical-align: middle; overflow: hidden;">
-                <span style="display: inline-block; width: 80px; font-size: 18px; margin-right: 10px; text-align: left; vertical-align: middle; white-space: nowrap;">
-                    Email
-                </span>
-                <div style="margin-right: 5px; vertical-align: middle;">:</div>
-                <a href="mailto:{{ $item['email_akun'] }}" style="text-decoration: none; color: inherit;">
-                    {{ $item['email_akun'] }}
-                </a>
-            </div>
-            <div class="list-item" style="margin: 10px 0; display: flex; align-items: center; justify-content: start; font-size: 16px; vertical-align: middle; overflow: hidden;">
-                <span style="display: inline-block; width: 80px; font-size: 18px; margin-right: 10px; text-align: left; vertical-align: middle; white-space: nowrap;">
-                    Password
-                </span>
-                <div style="margin-right: 5px; vertical-align: middle;">:</div>
+                <div class="divider" style="width: 100%; height: 1px; background-color: #FFC639; margin: 20px 0;"></div>
+                <div class="list-item"
+                    style="margin: 10px 0; display: flex; align-items: center; justify-content: start; font-size: 16px; vertical-align: middle; overflow: hidden;">
+                    <span
+                        style="display: inline-block; width: 80px; font-size: 18px; margin-right: 10px; text-align: left; vertical-align: middle; white-space: nowrap;">
+                        Judul
+                    </span>
+                    <div style="margin-right: 5px; vertical-align: middle;">:</div>
+                    <p
+                        style="flex: 1; margin: 0; text-align: left; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                        {{ $item['judul'] }}
+                    </p>
+                </div>
+                <div class="list-item"
+                    style="margin: 10px 0; display: flex; align-items: center; justify-content: start; font-size: 16px; vertical-align: middle; overflow: hidden;">
+                    <span
+                        style="display: inline-block; width: 80px; font-size: 18px; margin-right: 10px; text-align: left; vertical-align: middle; white-space: nowrap;">
+                        Harga
+                    </span>
+                    <div style="margin-right: 5px; vertical-align: middle;">:</div>
+                    <p
+                        style="flex: 1; margin: 0; text-align: left; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                        {{ Number::currency($item['harga'], in: 'IDR') }}
+                    </p>
+                </div>
+                <div class="list-item"
+                    style="margin: 10px 0; display: flex; align-items: center; justify-content: start; font-size: 16px; vertical-align: middle; overflow: hidden;">
+                    <span
+                        style="display: inline-block; width: 80px; font-size: 18px; margin-right: 10px; text-align: left; vertical-align: middle; white-space: nowrap;">
+                        UID
+                    </span>
+                    <div style="margin-right: 5px; vertical-align: middle;">:</div>
+                    <p
+                        style="flex: 1; margin: 0; text-align: left; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                        {{ $item['uid_akun'] }}
+                    </p>
+                </div>
+                <div class="list-item"
+                    style="margin: 10px 0; display: flex; align-items: center; justify-content: start; font-size: 16px; vertical-align: middle; overflow: hidden;">
+                    <span
+                        style="display: inline-block; width: 80px; font-size: 18px; margin-right: 10px; text-align: left; vertical-align: middle; white-space: nowrap;">
+                        Email
+                    </span>
+                    <div style="margin-right: 5px; vertical-align: middle;">:</div>
+                    <a href="mailto:{{ $item['email_akun'] }}" style="text-decoration: none; color: inherit;">
+                        {{ $item['email_akun'] }}
+                    </a>
+                </div>
+                <div class="list-item"
+                    style="margin: 10px 0; display: flex; align-items: center; justify-content: start; font-size: 16px; vertical-align: middle; overflow: hidden;">
+                    <span
+                        style="display: inline-block; width: 80px; font-size: 18px; margin-right: 10px; text-align: left; vertical-align: middle; white-space: nowrap;">
+                        Password
+                    </span>
+                    <div style="margin-right: 5px; vertical-align: middle;">:</div>
 
-                <p style="flex: 1; margin: 0; text-align: left; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
-                    {{ $item['password_akun'] }}
-                </p>
-            </div>
+                    <p
+                        style="flex: 1; margin: 0; text-align: left; overflow: hidden; white-space: nowrap; text-overflow: ellipsis;">
+                        {{ $item['password_akun'] }}
+                    </p>
+                </div>
             @endforeach
+            <div class="divider" style="width: 100%; height: 1px; background-color: #FFC639; margin: 20px 0;"></div>
+            <p
+                style="flex: 1; margin: 0; text-align: left; font-size: 20px; vertical-align: middle;
+                white-space: pre-wrap; overflow: hidden;
+                text-overflow: ellipsis;">{!! $deskripsi !!}</p>
         </div>
     </div>
 </body>
